54458a3076883c630ac6b98a5247692919eb7c4a
logannas <annacarolinafr36@gmail.com>
2021-04-26 22:16:41 -0300

    fix App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import { useState, useCallback, useMemo, useEffect } from 'react'
+import { useMemo} from 'react'
 import {
   Container,
-  Title,
-  Count,
-  LabelButton,
   Input,
   Label,
-  ContainInput,
   ContainForm,
   ButtonSearch,
   TextButton,
   Search,
 } from './styles'
-import { Button } from './components'
 import { useForm } from "react-hook-form";
-import axios from 'axios';
 import useFetch from 'use-http'
 
 const App = () => {
   // lidando com formularios
-  const { register, handleSubmit, watch, errors } = useForm();
+  const { register, handleSubmit, watch } = useForm();
 
 
   const {
@@ -27,13 +21,13 @@ const App = () => {
   } = useFetch('https://pokeapi.co/api/v2/pokemon', {}, [])
 
   const {
-    get, loading: loadingGet, error: errorGet, data: pokemon
+    get, error: errorGet, data: pokemon
   } = useFetch('https://pokeapi.co/api/v2/pokemon')
 
 
   const onSubmit = async form => {
     const response = await get(`/${form.nome}`)
-    console.log({ errorGet, pokemon })
+    console.log({ errorGet, pokemon, response })
   }
 
 
@@ -42,11 +36,11 @@ const App = () => {
 
   const showPokemons = useMemo(
     () => !watch('nome') && !error
-    , [error, watch('nome')])
+    , [error, watch])
 
   const showPokemon = useMemo(
     () => !!watch('nome') && !errorGet && pokemon
-    , [error, watch('nome'), pokemon])
+    , [errorGet, watch, pokemon])
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -80,30 +74,3 @@ const App = () => {
 export default App;
 
 
-/*
-const [pokemons, setPokemons] = useState([])
-
-  const requestPokemons = async () => {
-    try {
-      const response = await axios.get('https://pokeapi.co/api/v2/pokemon')
-      setPokemons(response.data.results)
-      console.log('response -> ', response);
-    } catch (error) {
-      console.log('error ->', error);
-    }
-  }
-
-  useEffect(() => {
-    requestPokemons()
-  }, [])
-   <Title>Form React</Title>
-        <ContainForm>
-          <Label>Nome</Label>
-          <Input ref={register} name='nome' />
-          <Label>Sobrenome</Label>
-          <Input ref={register} name='sobrenome' />
-        </ContainForm>
-        <Button type='submit'>
-          <LabelButton>Enviar</LabelButton>
-        </Button>
-*/
